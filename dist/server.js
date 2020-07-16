a3b3a2027db5e7b7ea518113adf705b5fa32c6dc
Stylll <stephen.aribaba@yahoo.com>
2020-07-16 04:33:29 -0600

    - add dist server - update readme file

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../build')));
+const port = process.env.PORT || 3000;
+
+// handle all react http requests
+app.get('/*', (request, response) => {
+  response.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.listen(port);
+console.log(`server started ${port}`);
